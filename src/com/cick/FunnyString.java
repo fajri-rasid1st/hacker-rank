3405ca52746a573f7279922dc0f42f98f70d0acd
fajri-rasid1st <fajrimuhammad460@gmail.com>
2020-04-02 18:40:12 +0700

    new update (4/2/2020)

---
+++ src/com/cick/FunnyString.java
@@ -0,0 +1,43 @@
+package com.cick;
+
+import java.util.Scanner;
+
+public class FunnyString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int queries = Integer.parseInt(sc.next());
+
+        while (queries > 0) {
+
+            String s = sc.next();
+            String reverse = "";
+            int[] arrS = new int[s.length() - 1];
+            int[] arrReverse = new int[s.length() - 1];
+            int total = 0;
+
+            for (int i = s.length() - 1; i > -1; i--) {
+                reverse += s.charAt(i);
+            }
+
+            for (int i = 0; i < s.length() - 1; i++) {
+                arrS[i] = Math.abs(s.codePointAt(i) - s.codePointAt(i + 1));
+                arrReverse[i] = Math.abs(reverse.codePointAt(i) - reverse.codePointAt(i + 1));
+            }
+
+            for (int i = 0; i < arrS.length; i++) {
+                if (arrS[i] == arrReverse[i]) {
+                    total++;
+                }
+            }
+
+            if (total == arrS.length) {
+                System.out.println("Funny");
+            } else {
+                System.out.println("Not Funny");
+            }
+            queries--;
+        }
+        sc.close();
+    }
+}
---
+++ src/com/cick/GameOfThrone.java
@@ -0,0 +1,42 @@
+package com.cick;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class GameOfThrone {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String[] sSorting = s.split("");
+        Arrays.sort(sSorting);
+
+        if (arrangedToPalindrome(sSorting)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
+    }
+
+    private static boolean arrangedToPalindrome(String s[]) {
+        int oddWord = 0;
+        int check = 1;
+        for (int i = 0; i < s.length - 1; i++) {
+            if (s[i].equals(s[i + 1])) {
+                check++;
+            } else {
+                if (check % 2 == 1) {
+                    oddWord++;
+                }
+                check = 1;
+            }
+        }
+        if (oddWord > 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
